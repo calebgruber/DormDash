fe3ed7e64c5554558ad27eb97e52a3c58d0de9db
Caleb Gruber <79333667+calebgruber@users.noreply.github.com>
2026-03-06 13:13:19 -0500

    Update APP_URL to production environment

--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php
 define('APP_NAME', 'DormDash');
-define('APP_URL', getenv('APP_URL') ?: 'http://localhost');
+define('APP_URL', getenv('APP_URL') ?: 'http://dormdash.dev.calebgruber.me');
 define('UPLOAD_DIR', __DIR__ . '/../uploads/');
 define('UPLOAD_URL', APP_URL . '/uploads/');
 define('PURCHASE_EMAIL_DOMAIN', '@purchase.edu');
